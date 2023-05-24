106b4aa2451441d1a76cb1b7234925d143e2ff68
fahmid111 <1805036@ugrad.cse.buet.ac.bd>
2023-05-24 06:08:53 +0600

    init

---
+++ src/test/java/com/fahmid/springboot_angular_mysql_intro/SpringbootAngularMysqlIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahmid.springboot_angular_mysql_intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAngularMysqlIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
